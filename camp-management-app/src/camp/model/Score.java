afbd3f5df75d2a85ea5eae65ad7b32a8befd56da
Daegyu Han <bigstar77han@gmail.com>
2024-08-07 13:37:33 +0900

    점수등록기능구현

--- camp-management-app/src/camp/model/Score.java
+++ camp-management-app/src/camp/model/Score.java
@@ -2,17 +2,46 @@ package camp.model;
 
 public class Score {
     private String scoreId; // 점수 ID
+    private Integer scoreNum; // 점수
     private Integer scoreRound; // 점수 회차 변수   ????? = xxxround
-    private String scoreGrade; // 점수 등급 변수
+    private Character scoreGrade; // 점수 등급 변수
+    private String scoreStudentId; // 수강생 ID
+    private String scoreSubjectId; // 과목 ID
 
-    public Score(String seq) {
-        this.scoreId = seq;
+    public Score(String scoreStudentId, String scoreSubjectId, Integer scoreRound, Integer scoreNum, Character scoreGrade) {
+        this.scoreStudentId = scoreStudentId;
+        this.scoreSubjectId = scoreSubjectId;
+        this.scoreRound = scoreRound;
+        this.scoreNum = scoreNum;
+        this.scoreGrade = scoreGrade;
     }
 
     // Getter
     public String getScoreId() {
         return scoreId;
     }
+
+    public String getScoreStudentId() {
+        return scoreStudentId;
+    }
+
+    public String getScoreSubjectId() {
+        return scoreSubjectId;
+    }
+
+    public Integer getScoreRound() {
+        return scoreRound;
+    }
+
+    public Integer getScoreNum() {
+        return scoreNum;
+    }
+
+    public Character getScoreGrade() {
+        return scoreGrade;
+    }
+
+
 
 }
 
